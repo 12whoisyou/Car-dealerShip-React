d107af1eef3404adc9a71d17759f6ba2ee4ee434
RedCube <Farouqjalabi@gmail.com>
2022-02-28 06:45:05 +0100

    Update CarPage.js

--- React App/src/components/CarPage.js
+++ React App/src/components/CarPage.js
@@ -1,29 +1,36 @@
 import React from "react";
+import EdiText from "react-editext";
 
-function CarPage(car) {
+import "./carpage.css";
+function CarPage({ car, cars, setCars }) {
+  const onSave = (val, self) => {
+    const updatedCars = cars.filter((i) => i.id !== car.id);
+    car[self.id] = val;
+    updatedCars.splice(car.id, 0, car);
+    console.log(car);
+    setCars(updatedCars);
+  };
+
+  var data =
+    "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(car));
+  /**       
+   * <a href={`data: ${data}`} download={"data.json"}>
+        Download JSON
+      </a>*/
   return (
     <div className="container-fluid row m-2">
       <div className="col-lg-6 row">
         <div className="container-fluid">
-          <img src={car.car.path}></img>
+          <img src={car.path}></img>
         </div>
 
         <div className="container-fluid">
-          <h1>{car.car.name}</h1>
-          <h2>{car.car.price + " 000 Kr"}</h2>
+          <h1>{car.name}</h1>
+          <h2>{car.price + " 000 Kr"}</h2>
         </div>
       </div>
-
-      <div className="col-lg-6 row">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed
-        augue lacus viverra vitae congue eu. Placerat orci nulla pellentesque
-        dignissim. Semper auctor neque vitae tempus quam pellentesque nec. Sed
-        pulvinar proin gravida hendrerit. Nullam non nisi est sit amet facilisis
-        magna. Hendrerit gravida rutrum quisque non tellus orci. Laoreet id
-        donec ultrices tincidunt arcu non sodales. Sodales neque sodales ut
-        etiam sit amet nisl purus. Tortor posuere ac ut consequat semper viverra
-        nam libero. Pellentesque nec nam aliquam sem et tortor.
+      <div className="container-fluid col-lg-6">
+        <p>{car.description}</p>
       </div>
     </div>
   );
