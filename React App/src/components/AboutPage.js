a52f60b8a8356338a1f3b3db558ff81d665cd850
RedCube <Farouqjalabi@gmail.com>
2022-02-28 06:45:09 +0100

    Update AboutPage.js

--- React App/src/components/AboutPage.js
+++ React App/src/components/AboutPage.js
@@ -4,7 +4,9 @@ function AboutPage() {
   return (
     <h2 className="m-3">
       A simple project to learn react. If your interested in the source code
-      check out
+      check out on{" "}
+      <a href="https://github.com/12whoisyou/Car-dealerShip-React">github</a> To
+      edit click the edit page and edit as you wish.
     </h2>
   );
 }
