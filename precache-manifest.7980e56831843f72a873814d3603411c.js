9854741b23c9902c464167c99325b8cfe3d77807
RedCube <Farouqjalabi@gmail.com>
2022-03-13 18:30:34 +0100

    Updates

---
+++ precache-manifest.7980e56831843f72a873814d3603411c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a1ab63e6d8fde0cd40c",
+    "url": "/Car-dealerShip-React/static/css/main.2ed432b9.chunk.css"
+  },
+  {
+    "revision": "2a1ab63e6d8fde0cd40c",
+    "url": "/Car-dealerShip-React/static/js/main.2a1ab63e.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/Car-dealerShip-React/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "8a9e7ac70f58bbb1c184",
+    "url": "/Car-dealerShip-React/static/css/2.c6f48618.chunk.css"
+  },
+  {
+    "revision": "8a9e7ac70f58bbb1c184",
+    "url": "/Car-dealerShip-React/static/js/2.8a9e7ac7.chunk.js"
+  },
+  {
+    "revision": "48902dea7827246ac7f4",
+    "url": "/Car-dealerShip-React/static/js/runtime~main.48902dea.js"
+  },
+  {
+    "revision": "3359fbbb5c2e57e2b2b5699ead9dc75e",
+    "url": "/Car-dealerShip-React/index.html"
+  }
+];
